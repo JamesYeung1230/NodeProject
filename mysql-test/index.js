ba65d0130487699fccc0aea90272f28d723af083
Peng YANG <yangpeng@harmonycloud.cn>
2022-11-03 19:21:27 +0800

    node鏈接MySQL

---
+++ mysql-test/index.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql')
+
+// 创建链接对象
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Peng@1230',
+    port: '3306',
+    database: 'myblog'
+})
+
+// 开始链接
+con.connect()
+
+// 执行sql语句
+const sql = 'select * from users;'
+con.query(sql, (err, result) => {
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log(result);
+})
+
+// 关闭链接
+con.end()
+
